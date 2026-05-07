b16013f30cd3114b9abb1222bfe19e758b630882
Procucev2022 <wesource@procucev.com>
2026-05-06 22:41:24 -0400

    Seller Reports

--- src/main/java/com/portal/procucev/Dto/SellerReportDto.java
+++ src/main/java/com/portal/procucev/Dto/SellerReportDto.java
@@ -2,38 +2,62 @@ package com.portal.procucev.Dto;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class SellerReportDto {
 
+    @JsonProperty("RfqDate")
     private Date rfqDate;
+
+    @JsonProperty("RfqId")
     private String rfqId;
+
+    @JsonProperty("SellerCompanyName")
     private String sellerCompanyName;
+
+    @JsonProperty("Email")
     private String email;
+
+    @JsonProperty("MobileNo")
     private String mobileNo;
+
+    @JsonProperty("CompanyLocation")
     private String companyLocation;
+
+    @JsonProperty("DeliveryLocation")
     private String deliveryLocation;
+
+    @JsonProperty("LastLogin")
     private Date lastLogin;
+
+    @JsonProperty("SourceType")
     private String sourceType;
+
+    @JsonProperty("RfqDownloadDate")
     private Date rfqDownloadDate;
+
+    @JsonProperty("QuoteSubmittedDate")
     private Date quoteSubmittedDate;
+
+    @JsonProperty("Query")
     private String query;
 
-    public SellerReportDto(
-            Date rfqDate,
-            String rfqId,
-            String sellerCompanyName,
-            String email,
-            String mobileNo,
-            String companyLocation,
-            String deliveryLocation,
-            Date lastLogin,
-            String sourceType,
-            Date rfqDownloadDate,
-            Date quoteSubmittedDate,
-            String query
-    ) {
+    public SellerReportDto(Date rfqDate,
+                           String rfqId,
+                           String sellerCompanyName,
+                           String email,
+                           String mobileNo,
+                           String companyLocation,
+                           String deliveryLocation,
+                           Date lastLogin,
+                           String sourceType,
+                           Date rfqDownloadDate,
+                           Date quoteSubmittedDate,
+                           String query) {
+
         this.rfqDate = rfqDate;
         this.rfqId = rfqId;
         this.sellerCompanyName = sellerCompanyName;
